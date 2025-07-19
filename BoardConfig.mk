0ec0972b8c1454fb1d2a2fe56355b6ceffd6678c
Momo5418 <momo5418@mocn.top>
2025-07-19 12:25:02 +0800

    Update

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,21 +1,13 @@
 #
-# Copyright (C) 2021 TeamWin Recovery Project
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 DEVICE_PATH := device/realme/RMX2173
 
+# For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
 
 # Architecture
@@ -46,8 +38,8 @@ TARGET_OTA_ASSERT_DEVICE := RMX2173,RMX2173CN,RMX2175,RMX2175CN
 # Kernel
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
-BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo
-TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
+TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
 BOARD_BOOT_HEADER_VERSION := 2
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_OFFSET := 0x00008000
--- README.md
+++ README.md
@@ -1,38 +1,10 @@
-# android_device_realme_RMX2173
-For building TWRP for realme Q2 Pro
-
-## Features
-
-Works:
-
-- ADB
-- Decryption of /data
-- Screen brightness settings
-- Vibration on touch
-
-## Compile
-
-First checkout minimal twrp with omnirom tree:
+# Android device tree for realme realme Q2 Pro 5G (RMX2173)
 
 ```
-repo init --depth=1 -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-11 --groups=all,-notdefault,-device,-darwin,-x86,-mips
-
-repo sync
-
-|or|
-
-repo sync --force-sync --no-clone-bundle --no-tags -j$(nproc --all)
-```
-
-Finally execute these:
-
-```
-export ALLOW_MISSING_DEPENDENCIES=true
-. build/envsetup.sh
-lunch twrp_RMX2173-eng
-mka -j$(nproc --all) recoveryimage
-
-|or|
-
-export ALLOW_MISSING_DEPENDENCIES=true; source build/envsetup.sh; lunch twrp_RMX2173-eng; mka -j$(nproc --all) recoveryimage
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
 ```
