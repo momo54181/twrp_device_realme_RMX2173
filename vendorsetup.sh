39f09c9161d6e9493801bc8bde237f0dc76456c2
Momo5418 <momo5418@mocn.top>
2025-08-02 20:22:18 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,27 +1,30 @@
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 OrangeFox Recovery Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2019-2025 The OrangeFox Recovery Project
+#	
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+# 	
+# 	Please maintain this if you use this script or any part of it
+#
 FDEVICE="RMX2173"
-#set -o xtrace
 
 fox_get_target_device() {
-local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
+local chkdev=$(echo "$BASH_SOURCE" | grep \"$FDEVICE\")
    if [ -n "$chkdev" ]; then
       FOX_BUILD_DEVICE="$FDEVICE"
    else
-      chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
+      chkdev=$(set | grep BASH_ARGV | grep \"$FDEVICE\")
       [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
    fi
 }
@@ -31,14 +34,27 @@ if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
 fi
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+	TOPDIR="$PWD";
+	FDEVICE_DIR=$(dirname ${BASH_ARGV[0]});
+	export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-v30.1.zip
+	export FOX_ENABLE_APP_MANAGER=1
+	export FOX_USE_BASH_SHELL=1
+	export FOX_ASH_IS_BASH=1
+	export FOX_USE_TAR_BINARY=1
+	export FOX_USE_SED_BINARY=1
+	export FOX_USE_XZ_UTILS=1
+	export FOX_USE_LZ4_BINARY=1
+	export FOX_USE_ZSTD_BINARY=1
+	export FOX_USE_DATE_BINARY=1
 
-# let's see what are our build VARs
-# Initial Exports
-	export ALLOW_MISSING_DEPENDENCIES=true
-	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
-    export LC_ALL="C"
+        # retrofitted dynamic partitions
+	export FOX_USE_DYNAMIC_PARTITIONS=1; # all builds now support dynamic partitions
+	if [ "$FOX_USE_DYNAMIC_PARTITIONS" = "1" ]; then
+   		export FOX_BASH_TO_SYSTEM_BIN=1; # install the bash binary to /system/bin/ instead of /sbin/
+		export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
+		export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
+	fi
 
-# Version / Maintainer infos.
 	export OF_MAINTAINER="Momo5418"
 	#export FOX_MAINTAINER_PATCH_VERSION="R11.1"
 	export FOX_BUILD_TYPE="Unofficial-Stable"
@@ -48,21 +64,4 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export OF_SCREEN_H="2400"
 # Custom pic for the maintainer's info in about section
 	export OF_MAINTAINER_AVATAR="/device/realme/RMX2173/maintainer.png"
-	
-# Device Information.
-	export FOX_ARCH=arm64
-	#export FOX_VARIANT="A12_FBE"
-	export TARGET_DEVICE_ALT="RMX2173" # I am not what are the over possible names.
-    export OF_KEEP_DM_VERITY_FORCED_ENCRYPTION=1
-
-    # export OF_SKIP_FBE_DECRYPTION_SDKVERSION= 30
-    export OF_DEFAULT_KEYMASTER_VERSION=4.1
-    export OF_FLASHLIGHT_ENABLE=0
-
-	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
-	  export | grep "FOX" >> $FOX_BUILD_LOG_FILE
-	  export | grep "OF_" >> $FOX_BUILD_LOG_FILE
-	  export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
-	  export | grep "TW_" >> $FOX_BUILD_LOG_FILE
-	fi
-fi		
+	export FOX_VANILLA_BUILD=1
