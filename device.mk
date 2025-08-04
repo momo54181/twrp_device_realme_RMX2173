114153b7b5525756641e95fb7f279e8f74751688
Momo5418 <momo5418@mocn.top>
2025-08-04 17:23:38 +0800

    尺寸

--- device.mk
+++ device.mk
@@ -6,18 +6,3 @@
 #
 
 LOCAL_PATH := device/realme/RMX2173
-
-
-# Shipping API level
-PRODUCT_SHIPPING_API_LEVEL := 29
-
-# Dynamic
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
-# Keystore
-PRODUCT_PACKAGES += \
-    android.system.keystore2
-
-# Keymaster
-PRODUCT_PACKAGES += \
-    android.hardware.keymaster@4.1
