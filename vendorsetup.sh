c0c3cebecfe8e7d9f1548053b4c88dbaa08322e8
Momo5418 <momo5418@mocn.top>
2025-08-04 20:47:55 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -41,7 +41,7 @@
 	export OF_MAINTAINER="Momo5418"
 	export FOX_BUILD_TYPE="Unofficial-Stable"
     export OF_STATUS_H="102"
-	export OF_STATUS_INDENT_LEFT="255"
+	export OF_STATUS_INDENT_LEFT="-155"
 	export OF_STATUS_INDENT_RIGHT="48"
 	export OF_SCREEN_H="2400"
 # Custom pic for the maintainer's info in about section
