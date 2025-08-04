245867877deb1c613f41264d424eabcc1c34ed69
Momo5418 <momo5418@mocn.top>
2025-08-04 20:43:07 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -127,6 +127,7 @@ TW_DEFAULT_LANGUAGE := zh_CN
 TW_INCLUDE_NTFS_3G := true
 TW_USE_TOOLBOX := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone3/temp
 TW_MAX_BRIGHTNESS := 4095
 TW_DEFAULT_BRIGHTNESS := 1700
 TW_NO_SCREEN_BLANK := true
